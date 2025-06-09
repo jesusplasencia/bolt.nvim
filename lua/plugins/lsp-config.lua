52653fbc520db8e1a9fb2c64592222347dffb290
Jesus Plasencia <jesustoledo2508@hotmail.com>
2025-06-09 07:55:10 -0500

    Autocompletion, automated installation of plugins for formattin and lintin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,11 +23,51 @@ return {
 		end,
 	},
 	{
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",  -- Depends on Mason
+      "nvimtools/none-ls.nvim",   -- Depends on null-ls
+    },
+    -- Use `ft` (filetype) to load this plugin only when relevant, improving startup
+    ft = {
+      "lua", "python", "javascript", "typescript", "css", "html", "json", "markdown",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        -- List the linters and formatters you want Mason to ensure are installed.
+        ensure_installed = {
+          "stylua",
+          "prettier",
+          "black",
+          "isort",
+          "eslint_d", -- Mason will install this executable
+        },
+        automatic_setup = true,
+        -- You can define custom handlers for these sources if needed,
+        -- similar to mason-lspconfig.
+        -- handlers = {
+        --   -- Example: Add custom arguments to prettier
+        --   prettier = function()
+        --     require("null-ls.builtins").formatting.prettier.with({
+        --       extra_args = { "--single-quote", "--jsx-single-quote" },
+        --     })
+        --   end,
+        -- },
+      })
+    end,
+  },
+  {
 		"neovim/nvim-lspconfig",
 		config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
