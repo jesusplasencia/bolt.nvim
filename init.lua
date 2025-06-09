e835c486baf6391fdaa9847953dc42dce3c39950
Jesus Plasencia <jesustoledo2508@hotmail.com>
2025-06-08 19:07:30 -0500

    Luke, I am your father!

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- ~/.config/nvim/init.lua
+
+local config_dir = vim.fn.stdpath("config") -- Get the path to your Neovim config directory
+
+vim.opt.rtp:prepend(config_dir) -- Add your config directory to the runtimepath
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.number = true         -- Corresponds to :set number
+vim.opt.relativenumber = true -- Corresponds to :set relativenumber
+
+-- Load lazy.nvim
+require("config.lazy")
